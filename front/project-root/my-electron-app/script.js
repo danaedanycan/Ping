bef0fff90fbc3d6a27e9a1c3fa9a3a8523f160ab
danae <danaedanycan@gmail.com>
2024-07-01 14:01:10 +0200

    save

--- front/project-root/my-electron-app/script.js
+++ front/project-root/my-electron-app/script.js
@@ -8,10 +8,11 @@ const terminalpanel = document.getElementById("terminal");
 const shortcutspanel = document.getElementById("shortcuts");
 const Commit_Button = document.getElementById("commit_button");
 const Push_Button = document.getElementById("push_button");
+const execute =  document.getElementById("execute_file")
 let current_project = null;
 let to_add = [];
 
-//requete git Status 
+//requete git Status
 async function GitStatus() {
     try {
         const response = await fetch('http://localhost:8080/api/execFeature/status', {
@@ -27,7 +28,7 @@ async function GitStatus() {
             try {
                 list = JSON.parse(result);
             } catch (error) {
-                terminal_input.value += ("\nFailed to parse string as JSON:", error);
+                terminal_input.value += ("\nFailed to parse string as JSON:"+ error);
                 alert("\nFailed to parse string as JSON: "+ error)
             }
             if (Array.isArray(list)) {
@@ -74,7 +75,7 @@ async function Git_ADD(path) {
         });
         const result = await response.text();
         if (response.ok) {
-            GitStatus();
+            await GitStatus();
             alert("ADD is succesfull We remove it from the files to add list.")
         } else {
             terminal_input.value += "\n" + result + "\n";
@@ -109,46 +110,45 @@ async function GitCommit(commit_mess) {
 }
 //Git tag with the tag message commit_mess
 async function GitTag(commit_mess) {
-  try {
-      const response = await fetch('http://localhost:8080/api/execFeature/Gittag', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ current_project: current_project, commit: commit_mess })
-      });
-      const result = await response.text();
-      if (response.ok) {
-          alert("Commit is successfuly tag!")
-      } else {
-          terminal_input.value += "\n" + result + "\n";
-          alert(result)
-      }
-  } catch (error) {
-      terminal_input.value += "\n" + error + "\n";
-      alert(error)
-  }
+    try {
+        const response = await fetch('http://localhost:8080/api/execFeature/Gittag', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ current_project: current_project, commit: commit_mess })
+        });
+        const result = await response.text();
+        if (response.ok) {
+            alert("Commit is successfuly tag!")
+        } else {
+            terminal_input.value += "\n" + result + "\n";
+            alert(result)
+        }
+    } catch (error) {
+        terminal_input.value += "\n" + error + "\n";
+        alert(error)
+    }
 }
 //push
 async function GitPush() {
-  try {
-      const response = await fetch('http://localhost:8080/api/execFeature/Gitpush', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body:current_project
-      });
-      const result = await response.text();
-      if (response.ok) {
-          alert("Push is Successfull! Congratulation !")
-      } else {
-        alert("Push isn't Successfull, maybe you need to PULL, or set correctly your git credentials")
-      }
-  } catch (error) {
-      terminal_input.value += "\n" + error + "\n";
-      alert(error)
-  }
+    try {
+        const response = await fetch('http://localhost:8080/api/execFeature/Gitpush', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:current_project
+        });
+        if (response.ok) {
+            alert("Push is Successfull! Congratulation !")
+        } else {
+            alert("Push isn't Successfull, maybe you need to PULL, or set correctly your git credentials")
+        }
+    } catch (error) {
+        terminal_input.value += "\n" + error + "\n";
+        alert(error)
+    }
 }
 
 //open a project and charge Files to add in GitWindow
@@ -167,7 +167,7 @@ async function Openproject(path) {
             current_project = path;
             terminal_input.value += "\n The project is correctly open !\n";
             alert("The project is correctly open !")
-            GitStatus();
+            await GitStatus();
         } else {
             terminal_input.value += "\n" + result+"\n";
             alert(result);
@@ -191,12 +191,11 @@ async function OpenFile(path) {
         if (response.ok) {
             const response = await fetch(result);
             if (response.ok) {
-                const text = await response.text();
-                code_display.value = text;
+                code_display.value = await response.text();
                 filename.innerHTML = result;
                 alert("File "+result+" is open in Code Window")
             } else {
-              terminal_input.value += "\n" + result + "\n";
+                terminal_input.value += "\n" + result + "\n";
                 alert(result)
             }
         } else {
@@ -219,11 +218,11 @@ async function CreateFile(path) {
         });
         const result = await response.text();
         if (response.ok) {
-            OpenFile(result);
+            await OpenFile(result);
             to_add.push(result);
             terminal_input.value += "\n The file is correctly created.\n";
             alert("The file is correctly created.")
-            GitStatus();
+            await GitStatus();
         } else {
             terminal_input.value += "\n" + result + "\n";
             alert(result)
@@ -252,7 +251,7 @@ async function DeleteFile(path) {
             if (to_add.find(path)) {
                 to_add = to_add.filter(item => item !== path)
             }
-            GitStatus();
+            await GitStatus();
         } else {
             terminal_input.value += "\n" + result + "\n";
             alert(result)
@@ -282,7 +281,7 @@ async function UpdateFile(path, content) {
         if (response.ok) {
             terminal_input.value += '\n' + 'The file is correctly updated.\n'
             alert("The file is correctly updated.")
-            GitStatus();
+            await GitStatus();
         } else {
             terminal_input.value += "\n" + result + "\n";
             alert(result)
@@ -330,7 +329,7 @@ async function DeleteFolder(path) {
         if (response.ok) {
             terminal_input.value += "\n The folder is correctly deleted.\n"
             alert("The folder is correctly deleted.")
-            GitStatus();
+            await GitStatus();
         } else {
             terminal_input.value += "\n" + result + "\n";
             alert(result)
@@ -355,7 +354,7 @@ async function Move(src, dst) {
         const result = await response.text();
         if (response.ok) {
             terminal_input.value += '\n' + 'The file is correctly updated.\n'
-            if (filename.innerHTML == src) {
+            if (filename.innerHTML === src) {
                 CloseFile();
             }
         } else {
@@ -378,8 +377,7 @@ async function loadDefaultFile() {
     try {
         const response = await fetch('terminal.txt');
         if (response.ok) {
-            const text = await response.text();
-            terminal_input.value = text;
+            terminal_input.value = await response.text();
         } else {
             console.error('Failed to fetch terminal.txt');
         }
@@ -444,11 +442,7 @@ async function Get_credentials() {
                 'Content-Type': 'application/json'
             },
         });
-        if (response.ok) {
-            return true;
-        } else {
-            return false;
-        }
+        return response.ok;
     } catch (error) {
         terminal_input.value += "\n" + error + "\n";
     }
@@ -463,11 +457,7 @@ async function Set_credentials(username, password) {
             },
             body: JSON.stringify({ identifiant: username, key: password })
         });
-        if (response.ok) {
-            return true;
-        } else {
-            return false;
-        }
+        return response.ok;
     } catch (error) {
         terminal_input.value += "\n" + error + "\n";
     }
@@ -479,27 +469,27 @@ async function IDE_EXEC() {
     const commands = test.split(' ');
     //pour open
     if (commands[0] === "open") {
-      //un project
+        //un project
         if (commands[1] === "project") {
-            Openproject(commands[2]);
+            await Openproject(commands[2]);
             //un file
         } else if (commands[1] === "file") {
-            OpenFile(commands[2]);
+            await OpenFile(commands[2]);
         }
         else {
             terminal_input.value += "\n" + "I can't open a " + commands[1] + " Did you want to say 'open project [absolute_path]' ?\nOr maybe 'open file [absolute_path]'"
 
-          } 
+        }
 
-          //pour créer
+        //pour créer
     } else if (commands[0] === "create") {
-      //file
+        //file
         if (commands[1] === "file") {
-            CreateFile(commands[2]);
+            await CreateFile(commands[2]);
         }
         //folder
         else if (commands[1] === "folder") {
-            CreateFolder(commands[2]);
+            await CreateFolder(commands[2]);
         }
         else {
             terminal_input.value += "\n" + "I can't create a " + commands[1] + " Did you want to say 'open folder [absolute_path]' ?\nOr maybe 'open file [absolute_path]'"
@@ -508,10 +498,10 @@ async function IDE_EXEC() {
     }
     else if (commands[0] === "delete") {
         if (commands[1] === "file") {
-            DeleteFile(commands[2]);
+            await DeleteFile(commands[2]);
         }
         else if (commands[1] === "folder") {
-            DeleteFolder(commands[2]);
+            await DeleteFolder(commands[2]);
         }
 
         else {
@@ -520,91 +510,114 @@ async function IDE_EXEC() {
         }
     }
     else if (commands[0] === "move") {
-        Move(commands[1], commands[2])
+        await Move(commands[1], commands[2])
     }
     else {
         terminal_input.value += "\n I dont' know this command, if you want to to know available commands enter 'help' and click on Ide Action"
     }
-    terminal_input.value += "\n"
+    terminal_input.value += "\n";
 }
-// actions 
+
+function exec(contentfile,filename) {
+  const exts = filename.split(".")
+    const extension = exts[-1];
+    terminal_input.value+=filename+"\nf,erifn,eri   "+extension+"\n"
+    if(extension==="js"){
+      terminal_input.value+="\nf,erifn,eri\n"
+        const output = document.getElementById("execute_result");
+        output.src = 'data:text/html;charset=utf-8,' + encodeURIComponent('<script>' + contentfile + '<\/script>');
+    }
+    else if(extension==="html"){
+        exec(`start "" "${filename}"`, (err) => {
+            if (err) {
+                alert('Erreur lors de l\'ouverture du fichier:'+err);
+            } else {
+                alert('Fichier ouvert avec succès');
+            }
+        });
+    }
+}
+// actions
 document.addEventListener('DOMContentLoaded', async () => {
-  // regarde si il y a des credentials
+    // regarde si il y a des credentials
     let cred = await Get_credentials();
-   
-        console.log('JavaScript loaded');
-        fullscreenDiv.classList.remove('active');
-        // execute la shell command
-        shell.addEventListener('click', async function () {
-            const test = getLastLine();
-            exec_terminal(test);
-        });
-        //execute l'ide command
-        ideButton.addEventListener('click', async function () {
-            IDE_EXEC();
-        });
 
-        //commit
-        Commit_Button.addEventListener('click', async function () {
-            const commit_mess = document.getElementById("commit_message")
-            GitCommit(commit_mess.value);
-        });
-        //push
-        Push_Button.addEventListener('click', async function () {
-          const tag_mess = document.getElementById("tag_message")
-          //tag si il y a un messag dans tag
-          if(tag_mess.value!=""  )
-              GitTag();
-          GitPush();
-      });
+    console.log('JavaScript loaded');
 
-      //Raccourcis
-        document.addEventListener('keydown', function (event) {
-            //Sauvegarde le fichier actuel dans CodeWindow
-            if (event.ctrlKey && event.key === 'S') {
-                const path = filename.innerHTML;
-                const content = code_display.value;
-                UpdateFile(path, content);
-            }
-            //Supprime le fichier actuel de Code Window
-            if (event.ctrlKey && event.key === 'D') {
-                event.preventDefault();
-                const path = filename.innerHTML;
-                DeleteFile(path)
-            }
-            //ouvre en grand CodeWindow
-            if (event.ctrlKey && event.shiftKey && event.key === 'C') {
-                event.preventDefault();
-                toggleFullScreen(codepanel);
-            }
-            //sort du plein écran
-            if (event.key === 'Escape') {
-                exitFullScreen();
-            }
-            //ouvre en grand Terminal Window
-            if (event.ctrlKey && event.shiftKey && event.key === 'T') {
-                event.preventDefault();
-                toggleFullScreen(terminalpanel);
-            }
-            //Ouvre en grand Shortcuts window
-            if (event.ctrlKey && event.shiftKey && event.key === 'S') {
-                event.preventDefault();
-                toggleFullScreen(shortcutspanel);
-            }
-            //ferme le fichier actuel dans Code Window
-            if (event.ctrlKey && event.key === 'K') {
-                event.preventDefault();
-                CloseFile();
-            }
-        });
+    // execute la shell command
+    shell.addEventListener('click', async function () {
+        const test = getLastLine();
+        await exec_terminal(test);
+    });
+    //execute l'ide command
+    ideButton.addEventListener('click', async function () {
+        await IDE_EXEC();
+    });
+
+    //commit
+    Commit_Button.addEventListener('click', async function () {
+        const commit_mess = document.getElementById("commit_message")
+        await GitCommit(commit_mess.value);
+    });
+    //push
+    Push_Button.addEventListener('click', async function () {
+        const tag_mess = document.getElementById("tag_message")
+        //tag si il y a un messag dans tag
+        if(tag_mess.value!==""  )
+            await GitTag();
+        await GitPush();
+    });
+    execute.addEventListener('click', async function () {
+        //tag si il y a un messag dans tag
+        exec(code_display.value,filename.innerHTML)
+    });
+    //Raccourcis
+    document.addEventListener('keydown', function (event) {
+        //Sauvegarde le fichier actuel dans CodeWindow
+        if (event.ctrlKey && event.key === 'S') {
+            const path = filename.innerHTML;
+            const content = code_display.value;
+            UpdateFile(path, content);
+        }
+        //Supprime le fichier actuel de Code Window
+        if (event.ctrlKey && event.key === 'D') {
+            event.preventDefault();
+            const path = filename.innerHTML;
+            DeleteFile(path)
+        }
+        //ouvre en grand CodeWindow
+        if (event.ctrlKey && event.shiftKey && event.key === 'C') {
+            event.preventDefault();
+            toggleFullScreen(codepanel);
+        }
+        //sort du plein écran
+        if (event.key === 'Escape') {
+            exitFullScreen();
+        }
+        //ouvre en grand Terminal Window
+        if (event.ctrlKey && event.shiftKey && event.key === 'T') {
+            event.preventDefault();
+            toggleFullScreen(terminalpanel);
+        }
+        //Ouvre en grand Shortcuts window
+        if (event.ctrlKey && event.shiftKey && event.key === 'S') {
+            event.preventDefault();
+            toggleFullScreen(shortcutspanel);
+        }
+        //ferme le fichier actuel dans Code Window
+        if (event.ctrlKey && event.key === 'K') {
+            event.preventDefault();
+            CloseFile();
+        }
+    });
 
 
-        //affiche le terminal par défault
-        loadDefaultFile();
+    //affiche le terminal par défault
+    await loadDefaultFile();
 
-        //Si il n'y a pas de crédentials de renseignés, les demandes
-        if (!cred) {
-        var div = document.getElementById('fullscreenDiv');
+    //Si il n'y a pas de crédentials de renseignés, les demandes
+    if (!cred) {
+        const div = document.getElementById('fullscreenDiv');
         div.classList.toggle('active');
         toggleFullScreen(document.getElementById('fullscreenDiv'));
         const username = document.getElementById("identifiant");
@@ -613,9 +626,9 @@ document.addEventListener('DOMContentLoaded', async () => {
             try {
                 const result = await Set_credentials(username.value, pasword.value);
                 if (result) {
-                    fullscreenDiv.classList.remove('active');
+                    div.classList.remove('active');
                     if (document.fullscreenElement) {
-                        document.exitFullscreen();
+                        await document.exitFullscreen();
                     }
                 } else {
                     console.log('Failed to set credentials');
